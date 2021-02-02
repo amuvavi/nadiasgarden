1e3c04945122fb9717b4c4affef641a4ca49549c
Austin Muvavi <amuvavi@gmail.com>
2021-02-02 10:48:57 +0200

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+class AdminController extends Controller
+{
+   public function menu(){
+   
+   $categories = Category::orderBy('display_order')->get();
+   return view('admin.menu-editor',[
+      'categories' => $categories
+   ]);
+   }
+}
---
+++ resources/js/components/MenuItem.vue
@@ -0,0 +1,3 @@
+<template>
+    <form>Add/Edit Item </form>
+</template>
